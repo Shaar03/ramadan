4c50c38d4188cdd345838cafa6cba98a3db6d378
Shaar.03 <ashaar409@gmail.com>
2024-08-04 11:52:40 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
-import Schedule from "./components/schedule/Schedule";
+import Schedule from "@/components/schedule/Schedule";
+import Login from "@/components/login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Schedule />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
