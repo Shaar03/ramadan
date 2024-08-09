d4c10434dae0ae918f1533a4c06a159c79e3f5ce
Shaar.03 <ashaar409@gmail.com>
2024-08-09 19:51:09 +0300

    add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import "./App.css";
 import Schedule from "@/components/schedule/Schedule";
 import Login from "@/components/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoutes from "@/utils/ProtectedRoutes";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/schedule" element={<Schedule />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/schedule" element={<Schedule />} />
+          </Route>
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
